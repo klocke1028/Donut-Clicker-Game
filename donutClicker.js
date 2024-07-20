58216a0f30d01befc794dc602884cf0ad9b914a7
klocke1028 <k.locke1028@gmail.com>
2024-07-19 23:40:01 -0400

    Commit

--- donutClicker.js
+++ donutClicker.js
@@ -1,42 +1,67 @@
 class donutClicker {
+    
     constructor() {
         this.buttonClicks = 0;
         this.donutCounter = 1;
 
         this.autoClickCounter = 0;
         this.autoClickerCost = 100;
+        this.autoClickerCount = 0;
+        this.autoClicksPerSec = 0;
+        this.buyAutoClickerButtonDisabled = true;
     }
 
-    donutButtonClicked() {
-        this.buttonClicks = this.buttonClicks + this.donutCounter;
+    makeADonut() {
+        this.buttonClicks += this.donutCounter;
     }
 
     getNumOfDonuts() {
-        return this.buttonClicks;
+        return Math.round(this.buttonClicks);
+    }
+
+    getNumOfDonutsPerClick() {
+        return this.donutCounter;
     }
 
     addAutoClicker() {
-        if (this.buttonClicks >= this.autoClickerCost) {
-            this.buttonClicks = this.buttonClicks - this.autoClickerCost
-            this.autoClickCounter++;
-            this.increaseAutoCost();
-        }
-    }
-
-    autoClickerFunction() {
-        this.buttonClicks = this.buttonClicks + this.autoClickCounter * this.buttonClicks;
+        this.autoClickCounter += 1;
+        this.buttonClicks -= this.autoClickerCost;
+        this.autoClicksPerSec += 1;
+        this.autoClickerCost = this.autoClickerCost += 20;
     }
 
     getAutoClickerNum() {
         return this.autoClickCounter;
     }
 
-    increaseAutoCost () {
-        this.autoClickerCost = this.autoClickerCost + (this.autoClickerCost + 20);
-    }
-
-    getCostOfAutoClicker () {
+    getAutoClickerCost () {
         return this.autoClickerCost;
     }
+
+    getAutoClicksPerSecond () {
+        return this.autoClicksPerSec
+    }
+
+    updateStoreButton() {
+        if (this.autoClickerCost <= this.buttonClicks){
+            this.buyAutoClickerButtonDisabled = false;
+        } else {
+            document.querySelector("#store-button").style.color = "rgb(100, 100, 100)";
+            this.buyAutoClickerButtonDisabled = true;
+        }
+    }
+
+    resetGame() {
+        this.buttonClicks = 0;
+        this.autoClickerCount= 0;
+        this.autoClickerCost = 100;
+        this.autoClicksPerSec = 0;
+        this.donutCounter = 1;
+        this.buyAutoClickerButtonDisabled = true;
+    }
+
+
+
+
 
 }
--- index.js
+++ index.js
@@ -1,14 +1,41 @@
+
+const totalDonuts = document.querySelector("#player-score");
+const makeDonutsButton = document.querySelector("#make-donuts");
+const totalAutoClickers = document.querySelector("#auto-clicks-purchased");
+const buyAcButton = document.querySelector("#store-button");
+const costOfAc = document.querySelector("#auto-clicker-cost");
+
 const fredDonutClicker = new donutClicker()
 
-const totalDonuts = document.querySelector("playerScore");
-const makeDonutsButton = document.querySelector("makeDonuts");
-const totalAutoClickers = document.querySelector("autoClicksPurchased");
-const buyAcButton = document.querySelector("storeButton");
-const costOfAc = document.querySelector("autoClickerCost");
-
-document.querySelector("#makeDonuts").onclick = donutButtonClicked() {
-    if (totalDonuts >= 0) {
-        totalDonuts ++;
-
-    }
+function gameDisplay() {
+    playerDonutCount.value = donutClicker.getNumOfDonuts();
+    autoClickersPurchased.value = donutClicker.getAutoClickerNum();
+    autoClickerCost.value = donutClicker.getAutoClickerCost();
+    donutsPerClick.value = donutClicker.getNumOfDonutsPerClick();
+    autoClickerButton.disabled = donutClicker.updateStoreButton();
 }
+
+gameDisplay();
+
+donutMakerButton.addEventListener("click", function() {
+    donutClicker.makeADonut();
+    gameDisplay();
+});
+
+buyAutoClicker.addEventListener("click", function() {
+   setInterval(function () {
+    const playerScore = document.querySelector("#player-score");
+    playerScore.innerText = counter;
+
+    counter++;
+    gameDisplay();
+   })
+}, 1000);
+
+resetGame.addEventListener("click", function() {
+    donutClicker.resetGame();
+    gameDisplay();
+
+})
+
+
